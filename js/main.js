53e058463443b39bda55893774e51d0adccb6bb3
Fidel Salgado <fidels@stanford.edu>
2016-02-11 15:29:32 -0800

    Added catodb

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-
+function main() {
+  cartodb.createVis('map', 'https://stanford.cartodb.com/u/fidelsalgado/api/v2/viz/a159608e-d112-11e5-96f5-0e31c9be1b51/viz.json', {
+      shareable: true,
+      title: true,
+      description: true,
+      search: true,
+      tiles_loader: true,
+      center_lat: 0,
+      center_lon: 0,
+      zoom: 2
+      })
+  .done(function(vis, layers) {
+    // layer 0 is the base layer, layer 1 is cartodb layer
+    // setInteraction is disabled by default
+    layers[1].setInteraction(true);
+    layers[1].on('featureOver', function(e, latlng, pos, data) {
+      cartodb.log.log(e, latlng, pos, data);
+    });
+    // you can get the native map to work with it
+    var map = vis.getNativeMap();
+    // now, perform any operations you need
+    // map.setZoom(3);
+    // map.panTo([50.5, 30.5]);
+  })
+  .error(function(err) {
+    console.log(err);
+  });
+}
+window.onload = main;
